39143f9312e86cbb82240bcf390cfc34968e8c46
Mesut Akyildiz <mesut.akyildiz@gmail.com>
2018-09-09 10:52:21 +0300

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,26 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  // socket.emit('newEmail', {
+  //   from: 'mesut@g.com',
+  //   text: 'How r u?',
+  //   createdAt: 4234
+  // });
+
+  // socket.on('createEmail', (email) => {
+  //   console.log('createEmail: ', email);
+  // });
+
+  socket.emit('newMessage', {
+    from: 'Mesut',
+    text: 'this is good',
+    createdAt: 234
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage: ', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('User disconnected from server');
   })
